e92bd74e90643ae2d5035cadc6f78129bdf08640
Carlos Marques <carlos.albmr@usp.br>
2023-06-24 11:37:34 -0300

    Feitas pequenas modificações de estilo na classe Explosion

--- app/src/main/kotlin/galaxyraiders/core/game/Explosion.kt
+++ app/src/main/kotlin/galaxyraiders/core/game/Explosion.kt
@@ -11,11 +11,11 @@ package galaxyraiders.core.game
 import galaxyraiders.core.physics.Point2D
 import galaxyraiders.core.physics.Vector2D
 
-class Asteroid(
+class Explosion(
   initialPosition: Point2D,
   initialVelocity: Vector2D,
   radius: Double,
   mass: Double,
   isTriggered: Boolean
 ) :
-  SpaceObject("Asteroid", 'x', initialPosition, initialVelocity, radius, mass)
+  SpaceObject("Explosion", 'x', initialPosition, initialVelocity, radius, mass)
